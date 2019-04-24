e0d3db921ed645cb26f8f4a1d216d70e991c862e
Deddy Kakunsi <dkakunsi@dgitsystems.com>
2019-04-24 19:08:30 +0800

    Issue #3 Add test.

--- jlogging/src/main/java/id/dkakunsi/lab/jlogging/layout/CustomJsonLayout.java
+++ jlogging/src/main/java/id/dkakunsi/lab/jlogging/layout/CustomJsonLayout.java
@@ -72,10 +72,6 @@ public class CustomJsonLayout extends AbstractStringLayout {
 
     private static final String DEFAULT_ATTRIBUTES = "timestamp,category,level,message";
 
-    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm'Z'";
-
-    private static final String DEFAULT_TIMEZONE = "UTC";
-
     private String dateFormat;
 
     private String timezone;
@@ -112,8 +108,8 @@ public class CustomJsonLayout extends AbstractStringLayout {
     public static CustomJsonLayout createLayout(
             @PluginAttribute(value = "charset", defaultString = "UTF-8") Charset charset,
             @PluginAttribute(value = "isRecursiveStackTrace", defaultBoolean = true) boolean recursiveStacktrace,
-            @PluginAttribute(value = "timezone", defaultString = DEFAULT_TIMEZONE) String timezone,
-            @PluginAttribute(value = "dateFormat", defaultString = DEFAULT_DATE_FORMAT) String dateFormat,
+            @PluginAttribute(value = "timezone") String timezone,
+            @PluginAttribute(value = "dateFormat") String dateFormat,
             @PluginAttribute(value = "attributes", defaultString = DEFAULT_ATTRIBUTES) String attributes) {
         return new CustomJsonLayout(charset, recursiveStacktrace, timezone, dateFormat, attributes);
     }
@@ -140,14 +136,21 @@ public class CustomJsonLayout extends AbstractStringLayout {
 
         Map<String, Object> format = new LinkedHashMap<>();
         for (String attribute : getAttributes()) {
-            format.put(attribute, selectLoader(attribute).load(attribute));
+
+            Object attributeValue = selectLoader(attribute).load(attribute);
+            if (attributeValue != null) {
+                format.put(attribute, attributeValue);
+            }
         }
 
+        String response;
         try {
-            return mapper.writeValueAsString(format);
+            response = mapper.writeValueAsString(format);
         } catch (JsonProcessingException ex) {
-            return ex.getMessage();
+            response = "{\"error\": \"Unable to create message\"}";
         }
+
+        return String.format("%s\n", response);
     }
 
     private String[] getAttributes() {
@@ -161,7 +164,6 @@ public class CustomJsonLayout extends AbstractStringLayout {
             }
         }
 
-        // this will always return null;
         return this.attributeLoaders[0];
     }
 }
